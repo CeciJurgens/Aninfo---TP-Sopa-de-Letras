ce848961924b6d32d950a61eea9ca86b6f31f835
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-12-07 18:18:12 -0300

    Update Palabra.java

--- src/main/java/modelo/Clases/Palabra.java
+++ src/main/java/modelo/Clases/Palabra.java
@@ -1,46 +1,39 @@
 package modelo.Clases;
+import java.util.ArrayList;
 
 public class Palabra{
 
-    private int coordenadaX;
-    private int coordenadaY;
+    private ArrayList<int> coordenadasInicio;
+    private ArrayList<int> coordenadasFin;
     private boolean encontrada;
     private boolean longitudValida;
 
-    public Palabra(int x, int y, int filasTablero, int columnasTablero) {
-        this.coordenadaX = x;
-        this.coordenadaY = y;
+    public Palabra(ArrayList<int> coordsInicio, ArrayList<int> coordsFin) {
+        this.coordenadasInicio = coordsInicio;
+        this.coordenadasFin = coordsFin;
         this.encontrada = false;
-        this.longitudValida= longitudEsValida(filasTablero, columnasTablero);
     }
-
+    
     //TODO: revisar en refactor
-    private boolean longitudEsValida(int filasTablero, int columnasTablero){
-        return this.coordenadaX >= 0 && this.coordenadaX <= filasTablero && this.coordenadaY >= 0 && this.coordenadaY <= columnasTablero
-    }
-
-    //TODO: revisar en refactor
-    private boolean actualizacionEstadoPalabra(int coordSeleccionadaX, int coordSeleccionadaY){
-        boolean estadoPalabra = this.encontrada
-        if  (this.coordenadaX == coordSeleccionadaX && this.coordenadaY == coordSeleccionadaY){
+    private boolean actualizacionEstadoPalabra(ArrayList<int> coordsSeleccionadasInicio, ArrayList<int> coordsSeleccionadasFin){
+        boolean estadoPalabra = this.encontrada;
+        if (this.coordenadasInicio == coordsSeleccionadasInicio && this.coordenadasFin == coordsSeleccionadasFin){
             this.encontrada = true
         }
         return  estadoPalabra != this.encontrada
     }
+
     //TODO: borrar funciones, provisorias para chequeos.
-    public void getCoordenadaX(){
-        return this.coordenadaX
+    public void getCoordenadasInicio(){
+        return this.coordenadasInicio
     }
 
-    public void getCoordenadaY(){
-        return this.coordenadaY
+    public void getCoordenadaFin(){
+        return this.coordenadasFin
     }
 
     public void getStatusPalabra(){
         return this.encontrada
     }
 
-    public void getValidezLongitud(){
-        return this.longitudValida
-    }    
 }

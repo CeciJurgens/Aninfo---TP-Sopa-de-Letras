0c9707ee4127a51fa890848065e5631ca06d4da3
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-12-08 19:25:30 -0300

    Updates
    Creation of PalabraTest.
    Update of attribute and getter of such attribute.

---
+++ src/main/Test/PalabraTest.java
@@ -0,0 +1,31 @@
+import modelo.Clases.Palabra;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PalabraTest{
+    @BeforeEach
+    public void setUp(){
+        //instanciar palabra 
+    }
+
+    @Test
+    public void Test02GetEstadoPalabra(){
+        //testeo getter estado palabra default
+    }
+
+    @Test
+    public void Test03actualizacionEstadoPalabra(){
+        //testeo actualizacionEstadoPalabra
+    }
+
+    @Test
+    public void Test04GetCoordenadasInicio(){
+        //testeo gettter coordenadas Inicio
+    }
+
+    @Test
+    public void Test05GetCoordenadasFin(){
+        //testeo getter coordenadas Fin
+    }
+
+}

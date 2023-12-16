e844f1225689582cfbd35707a135f30ce6eaf54f
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-12-16 16:29:01 -0300

    Update Juego.java

--- src/main/java/modelo/Clases/Juego.java
+++ src/main/java/modelo/Clases/Juego.java
@@ -28,7 +28,7 @@ public class Juego{
 
     private void colocarPalabrasEnTablero(List<Palabra> listaPalabras, int cantidadPalabras){
         for(i=0; i<cantidadPalabras ; i++){
-            this.tablero.asignarCoordenadasPalabras(listaPalabras[i],rd.nextInt());
+            this.tablero.asignarCoordenadasPalabras(listaPalabras[i],rd.nextInt(), rd.nextInt());
             i++;
         }
     }

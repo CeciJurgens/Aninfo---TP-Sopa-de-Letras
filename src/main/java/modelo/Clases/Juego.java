d7ecd8c503e2b0ecac288122e0cf8e42ffa0228a
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-12-16 13:30:32 -0300

    Update Juego.java
    Addition of boolean seHaGanado attribute

--- src/main/java/modelo/Clases/Juego.java
+++ src/main/java/modelo/Clases/Juego.java
@@ -6,9 +6,11 @@ public class Juego{
 
     private Tablero tablero;
     private CategoriaDePalabras categoriaPalabras;
+    private boolean seHaGanado;
 
     public Juego() {
         this.tablero = new Tablero(15,20); //tablero 15x20
         this.categoriaPalabras = new CategoriaDePalabras();
+        this.seHaGanado = false;
     }
 }

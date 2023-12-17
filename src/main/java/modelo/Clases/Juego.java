a3145ace117c5d83c56e6d19bf550e6d29f54beb
ppazb <ppaz@fi.uba.ar>
2023-12-17 02:53:21 -0300

    Representacion de tablero y chequeo de encontrar palabras
    - Tablero cambia color de aquellos casilleros que tienen letras que pertenecen a palabras encontradas
    - Juego recibe coordenadas y determina si se encontro alguna palabra
    - Juego ahora puede chequear si se gano una partida

--- src/main/java/modelo/Clases/Juego.java
+++ src/main/java/modelo/Clases/Juego.java
@@ -1,4 +1,5 @@
 package modelo.Clases;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -7,30 +8,56 @@ public class Juego{
 
     private Tablero tablero;
     private String[] coleccionPalabras;
-    private boolean seHaGanado;
     private List<Palabra> palabrasJuego;
 
     //Sugiero agregar a obtenerCollecion un parametro que indique logitud max por c/palabra de la coleccion.
     public Juego(String unaCategoriaPalabras, int cantidadPalabras){
-        this.tablero = new Tablero(15,20); //tablero 15x20
+        this.tablero = new Tablero(15,15); //tablero 15x20
         this.coleccionPalabras = new CategoriaDePalabras().obtenerColeccion(unaCategoriaPalabras, cantidadPalabras);
-        this.seHaGanado = false;
-        Random rd = new Random();
         this.palabrasJuego = listadoIntanciasPalabra(cantidadPalabras);
         colocarPalabrasEnTablero(this.palabrasJuego, cantidadPalabras);
     }
 
     private List<Palabra> listadoIntanciasPalabra(int cantidadPalabras){
         List<Palabra> palabras = new ArrayList<>();
-        for (i=0; i<cantidadPalabras; i++){
+        for (int i=0; i<cantidadPalabras; i++){
+            Random rd = new Random();
             palabras.add(new Palabra(this.coleccionPalabras[i],rd.nextBoolean()));
         }
         return palabras;
     }
 
     private void colocarPalabrasEnTablero(List<Palabra> listaPalabras, int cantidadPalabras){
-        for(i=0; i<cantidadPalabras ; i++){
-            this.tablero.asignarCoordenadasPalabras(listaPalabras[i],rd.nextInt(), rd.nextInt());
+        for(int i=0; i<cantidadPalabras ; i++){
+            Random rd = new Random();
+            this.tablero.asignarCoordenadasPalabras(listaPalabras.get(i),rd.nextInt(),rd.nextInt());
         }
+    }
+
+    public boolean ganoJuego(){
+        //Precondicion: existe al menos una palabra en juego
+        boolean gano = true;
+        for (Palabra palabra: this.palabrasJuego) {
+            if (palabra.getEstadoPalabra() == false){gano = false;}
+        }
+        return gano;
+    }
+
+    public boolean perdioJuego(){
+        //TODO Si agregamos condiciones para perder u tiempo aca va un if tiempo < 0 -> perdio.
+        return false;
+    }
+
+    public boolean encontrarPalabra(List<Integer> coordenadaInicio, List<Integer> coordenadaFin){
+        for (Palabra palabra: this.palabrasJuego) {
+            if (palabra.actualizacionEstadoPalabra(coordenadaInicio,coordenadaFin) == true ||
+            palabra.actualizacionEstadoPalabra(coordenadaFin,coordenadaInicio) == true){
+                this.tablero.cambiarEstadoCasilleros(palabra.getCoordenadasInicio(),palabra.getCoordenadasFin());
+                return true;
+
+            }
+            //No importa en q orden da el jugador las coordenadas
+        }
+        return false;
     }
 }

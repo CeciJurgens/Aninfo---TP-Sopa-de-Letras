201fa398242c435d3aafc2d9b9efaaf187fc59bf
Ceci Jurgens <cjurgens@fi.uba.ar>
2023-12-17 09:07:48 -0300

    Add files via upload

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -1,6 +1,23 @@
 package modelo;
 import modelo.Clases.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Juego juego = new Juego("FRUTAS", 5);
+
+        for (Palabra palabra : juego.getPalabrasJuego()) {
+            System.out.println(palabra);
+        }
+
+        String input = "1 2\n3 4\n"; // Coordenadas simuladas
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        juego.pedirCoordenadasPantalla(in);
+    }
 
 }

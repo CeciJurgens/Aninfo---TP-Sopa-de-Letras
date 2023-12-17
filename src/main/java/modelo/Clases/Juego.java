1a49196bd478fbba2fb520bfe283f22fde65f347
Ceci Jurgens <cjurgens@fi.uba.ar>
2023-12-17 09:08:26 -0300

    Add files via upload

--- src/main/java/modelo/Clases/Juego.java
+++ src/main/java/modelo/Clases/Juego.java
@@ -1,7 +1,9 @@
 package modelo.Clases;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Juego{
@@ -30,7 +32,7 @@ public class Juego{
     private void colocarPalabrasEnTablero(List<Palabra> listaPalabras, int cantidadPalabras){
         for(int i=0; i<cantidadPalabras ; i++){
             Random rd = new Random();
-            this.tablero.asignarCoordenadasPalabras(listaPalabras.get(i),rd.nextInt(),rd.nextInt());
+            this.tablero.asignarCoordenadasPalabras(listaPalabras.get(i),rd.nextInt(14 - 1 + 1) + 1, rd.nextInt(14 - 1 + 1) + 1);
         }
     }
 
@@ -51,8 +53,8 @@ public class Juego{
     public boolean encontrarPalabra(List<Integer> coordenadaInicio, List<Integer> coordenadaFin){
         for (Palabra palabra: this.palabrasJuego) {
             if (palabra.actualizacionEstadoPalabra(coordenadaInicio,coordenadaFin) == true ||
-            palabra.actualizacionEstadoPalabra(coordenadaFin,coordenadaInicio) == true){
-                this.tablero.cambiarEstadoCasilleros(palabra.getCoordenadasInicio(),palabra.getCoordenadasFin());
+                    palabra.actualizacionEstadoPalabra(coordenadaFin,coordenadaInicio) == true){
+                this.tablero.cambiarEstadoCasilleros(palabra.getIsEsHorizontal(), palabra.getCoordenadasInicio(),palabra.getCoordenadasFin());
                 return true;
 
             }
@@ -60,4 +62,73 @@ public class Juego{
         }
         return false;
     }
+
+    public List<Palabra> getPalabrasJuego() {
+        return palabrasJuego;
+    }
+
+    private boolean coordenadasDentroDeRango(List<Integer> coordenadas) {
+        int filasMaximas = 15;
+        int columnasMaximas = 15;
+
+        return coordenadas.get(0) >= 0 && coordenadas.get(0) < filasMaximas &&
+                coordenadas.get(1) >= 0 && coordenadas.get(1) < columnasMaximas;
+    }
+
+
+    public void pedirCoordenadasPantalla(InputStream inputStream){
+        Scanner scanner = new Scanner(System.in);
+
+        tablero.mostrarTablero();
+
+        while (!ganoJuego()) {
+            boolean coordenadasValidas = false;
+            List<Integer> coordenadaInicio = null;
+            List<Integer> coordenadaFin = null;
+
+            while (!coordenadasValidas) {
+                try {
+                    System.out.println("Ingresa las coordenadas de inicio (fila columna):");
+                    int filaInicio = scanner.nextInt();
+                    int columnaInicio = scanner.nextInt();
+
+                    System.out.println("Ingresa las coordenadas de fin (fila columna):");
+                    int filaFin = scanner.nextInt();
+                    int columnaFin = scanner.nextInt();
+
+                    coordenadaInicio = List.of(filaInicio, columnaInicio);
+                    coordenadaFin = List.of(filaFin, columnaFin);
+
+                    // Verificar si las coordenadas están dentro del rango permitido
+                    if (coordenadasDentroDeRango(coordenadaInicio) && coordenadasDentroDeRango(coordenadaFin)) {
+                        coordenadasValidas = true;
+                    } else {
+                        System.out.println("¡Coordenadas fuera de rango! Ingresa nuevamente.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error al ingresar coordenadas. Asegúrate de ingresar números enteros.");
+                    scanner.nextLine(); // Limpiar el búfer del scanner para evitar un bucle infinito
+                }
+            }
+
+            boolean palabraEncontrada = encontrarPalabra(coordenadaInicio, coordenadaFin);
+
+            tablero.mostrarTablero();
+
+            if (palabraEncontrada) {
+                System.out.println("¡Palabra encontrada!");
+            } else {
+                System.out.println("No se encontró ninguna palabra con esas coordenadas.");
+            }
+
+            for (Palabra palabra : getPalabrasJuego()) {
+                if (!palabra.getEstadoPalabra()) {
+                    System.out.print(palabra.getPalabraString() + " ");
+                }
+            }
+        }
+
+        System.out.println("¡Felicidades! Has encontrado todas las palabras.");
+    }
+
 }

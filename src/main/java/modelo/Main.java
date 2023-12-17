0e5b7e9fba3a9160314f44c4c381271153e07c03
Melulatana <78887873+Melulatana@users.noreply.github.com>
2023-12-17 15:48:20 -0300

    Agrego la funcionalidad del time en la Clase Juego
    El tiempo es el nuevo requisito del jugador para determinar si puede llegar a ganar o perder dependiendo del escenario que se encuentre.

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -4,10 +4,14 @@ import modelo.Clases.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+
+
 public class Main {
+
 
     public static void main(String[] args) {
 
+        System.out.println("BIENVENIDO A LA SOPA DE LETRAS");
         Juego juego = new Juego("FRUTAS", 5);
 
         for (Palabra palabra : juego.getPalabrasJuego()) {
@@ -18,6 +22,13 @@ public class Main {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         juego.pedirCoordenadasPantalla(in);
+
+        // Esperar unos segundos para mostrar el tiempo actualizado antes de que el programa finalice
+        try{
+            Thread.sleep(5000); // Espera 5 segundos
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
 }
